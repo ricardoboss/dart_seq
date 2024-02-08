9bb65ea06231e26a764f8244a5a43fc0849ab6a7
Ricardo Boss <mail@ricardoboss.de>
2024-02-08 23:57:56 +0100

    Make SeqClientException extend Error

--- lib/src/seq_client_exception.dart
+++ lib/src/seq_client_exception.dart
@@ -2,16 +2,13 @@ import 'package:dart_seq/dart_seq.dart';
 
 /// Exceptions thrown by [SeqClient] implementations when they fail to send
 /// one or more events to the Seq server.
-class SeqClientException {
+class SeqClientException extends Error {
   /// Creates a [SeqClientException] with the given message.
-  SeqClientException(this.message, [this.innerException, this.stackTrace]);
+  SeqClientException(this.message, [this.innerException]);
 
   /// The exception message.
   final String message;
 
   /// The exception that caused this exception, if any.
   final Object? innerException;
-
-  /// The stack trace of the exception, if any.
-  final StackTrace? stackTrace;
 }
